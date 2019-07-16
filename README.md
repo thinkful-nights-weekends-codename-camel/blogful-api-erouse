2df85d6a866e501b9f5dcbda575586bcfb7e9bfe
Emanuel Rouse <emanuelrouse@users.noreply.github.com>
2019-07-16 16:05:34 -0400

    second commit

--- README.md
+++ README.md
@@ -21,6 +21,8 @@ Start nodemon for the application `npm run dev`
 
 Run the tests `npm test`
 
+Run migrations `npm run migrate`
+
 ## Deploying
 
 When your new project is ready for deployment, add a new Heroku application with `heroku create`. This will make a new git remote called "heroku" and you can then `npm run deploy` which will push to this remote's master branch.
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const ArticlesService = require('./articles-service')
 const errorHandler = require('./error-handler')
 const app = express();
 
@@ -14,6 +15,21 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
+
+app.get('/articles', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+    ArticlesService.getAllArticles(knexInstance)
+      .then(articles => {
+        res.json(articles.map(article => ({
+          id: article.id,
+          title: article.title,
+          style: article.style,
+          content: article.content,
+          date_published: new Date(article.date_published),
+        })))
+      })
+      .catch(next)
+})
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
